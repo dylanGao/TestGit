6833b68d37ceae4af3c512fb617cac8e9a190ec0
ZwjNOTNull <zhuwenjin@jacketzhong.local>
2016-06-13 16:28:47 +0800

    no message

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
      var alertView = require('UIAlertView').alloc().init();
-     alertView.setTitle('Warning');
-     alertView.setMessage('JSPatch is comming！'); 
+     alertView.setTitle('警告');
+     alertView.setMessage('JS正在靠近！'); 
      alertView.addButtonWithTitle('OK');
      alertView.show();
      
